8440860d83c953d345490fb5b9f37de642034973
Anjolaoluwa Akindipe <anjyakindipe@gmail.com>
2020-05-29 02:40:24 +0100

    committing the memory cleaner ui

---
+++ android/app/src/main/kotlin/com/example/memory_cleaner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.memory_cleaner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:memory_cleaner/memory_cleaner.dart';
+import 'package:memory_cleaner/memory_cleaner_appbar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MemoryCleanerAppBar(),
+      body: Practice(),
+    );
+  }
+}
